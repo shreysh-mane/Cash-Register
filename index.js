c4eb3c56c21a37ff6b564e5d4be3bc06798d3e41
shreysh-mane <shreysh.mane12@gmail.com>
2021-08-20 00:59:29 +0530

    feat- cash calculation

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
 const bill = document.querySelector("#bill");
 const amount = document.querySelector("#amount");
-const check = document.querySelector("#check");
-const display = document.querySelector("#display");
-const table = document.querySelector("#table");
+const check = document.querySelector("#btn-check");
+const next = document.querySelector("#btn-next");
+const table = document.querySelector("#table-display");
 const displayMessge = document.querySelector("#message");
+const notes = document.querySelectorAll(".notes");
+const eAmount = document.querySelector("#enterAmount");
 
 let noteArray = [2000, 500, 100, 50, 10, 5, 1];
 let billAmount;
 let amountGiven;
 let remainingAmount;
 let message;
+const shownext = () => {
+  next.style.display = "none";
+  eAmount.style.display = "block";
+  check.style.display = "block";
+};
+next.addEventListener("click", shownext);
+const show = (msg) => {
+  // display.style.display = "block";
 
-// const show = (msg) => {
-//   display.style.display = "block";
+  if (msg == "Invalid") {
+    displayMessge.style.display = "block";
+    displayMessge.innerHTML = "ENTER CORRECT AMOUNT";
+  } else if (msg == "lessAmount") {
+    displayMessge.style.display = "block";
+    displayMessge.innerHTML = "YOU NEED MORE MONEY";
+  } else {
+    table.style.display = "block";
+    for (i in notes) {
+      notes[i].innerHTML = `${Math.trunc(remainingAmount / noteArray[i])}`;
+      remainingAmount = remainingAmount % noteArray[i];
+    }
+  }
+};
 
-//   if (msg == "Invalid") {
-//     displayMessge.innerHTML = "ENTER CORRECT AMOUNT";
-//     displayMessge.style.display = "block";
-//   } else if (msg == "lessAmount") {
-//     displayMessge.innerHTML = "You need more money";
-//     displayMessge.style.display = "block";
-//   } else {
-//     table.style.display = "block";
-//   }
-// };
+const getValues = () => {
+  displayMessge.style.display = "none";
+  table.style.display = "none";
 
-// const getValues = () => {
-//   displayMessge.style.display = "none";
-//   table.style.display = "none";
+  billAmount = Number(bill.value);
+  amountGiven = Number(amount.value);
+  if (billAmount < 0 || amountGiven < 0) {
+    message = "Invalid";
+  } else if (billAmount > amountGiven) {
+    message = "lessAmount";
+  } else {
+    message = "calculte";
+    remainingAmount = amountGiven - billAmount;
+  }
 
-//   billAmount = Number(bill.value);
-//   amountGiven = Number(amount.value);
-//   if (billAmount < 0 || amountGiven < 0) {
-//     message = "Invalid";
-//   } else if (billAmount > amountGiven) {
-//     message = "lessAmount";
-//   } else {
-//     message = "calculte";
-//     remainingAmount = amountGiven - billAmount;
-//   }
+  show(message);
+};
 
-//   show(message);
-// };
-
-// check.addEventListener("click", getValues);
+check.addEventListener("click", getValues);
